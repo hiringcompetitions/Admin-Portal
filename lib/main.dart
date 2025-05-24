59d842c533b8bb629d2f4fbc78ec18d4c2dd9490
gummala-sandeep <gummalasandy191212@gmail.com>
2025-05-24 13:22:58 +0530

    Oppurtunity card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_side_menu/flutter_side_menu.dart';
-
+import 'package:hiring_competitions_admin_portal/backend/providers/category_provider.dart';
 import 'package:hiring_competitions_admin_portal/backend/providers/custom_auth_provider.dart';
 import 'package:hiring_competitions_admin_portal/backend/providers/firestore_provider.dart';
+import 'package:hiring_competitions_admin_portal/constants/theme.dart';
 import 'package:hiring_competitions_admin_portal/firebase_options.dart';
 import 'package:hiring_competitions_admin_portal/views/auth/login.dart';
 import 'package:hiring_competitions_admin_portal/views/auth/signup.dart';
+import 'package:hiring_competitions_admin_portal/views/oppurtunities/add_opportunity_card.dart';
+import 'package:hiring_competitions_admin_portal/views/oppurtunities/oppurtunities.dart';
 import 'package:hiring_competitions_admin_portal/views/sidebar.dart';
 import 'package:provider/provider.dart';
 
